5fb29d8bdc7f718f3d8a58c8edc375cfbbbc2e76
Jahid Hasan <jahidhasan2856@gmail.com>
2022-01-28 19:45:16 +0600

    some update

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -1,11 +1,15 @@
-import NekoWorld from "../neko-world/NekoWorld";
+import NekoAttribute from "../nekoAttribute/nekoAttribute";
+import NekoElement from "../nekoElement/NekoElement";
+import NekoWorld from "../nekoWorld/NekoWorld";
 
 const MainSection = () => {
     return (
         <>
             <div className="other-section-content">
-
                 <NekoWorld></NekoWorld>
+              {/* <NekoElement></NekoElement> */}
+              {/* <NekoAttribute></NekoAttribute> */}
+                
             </div>
         </>
     );
--- src/components/TopHeader/TopHeader.js
+++ src/components/TopHeader/TopHeader.js
@@ -8,13 +8,19 @@ const TopHeader = () => {
         <>
             <section className="mb-5 overflow-hidden top-section">
                 <div className="">
-                
+
                     <div className="wrapper d-flex justify-content-center position-relative">
-                        <div className="container pt-5 pt-sm-0" style={{position: 'absolute'}}>
+                        <div className="container pt-5 pt-sm-0" style={{ position: 'absolute' }}>
                         </div>
-                        <Image className="circle-spin" src={BigCircle} />
-                        <Image className="circle-spin-r" src={smCircle} />
-                        <Image className="img-main" src={Main} />
+                        <div className="circle-spin">
+                            <Image src={BigCircle} />
+                        </div>
+                        <div className="circle-spin-r">
+                            <Image src={smCircle} />
+                        </div>
+                        <div className="img-main">
+                            <Image src={Main} />
+                        </div>
                     </div>
                 </div>
             </section>
