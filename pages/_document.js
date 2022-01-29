958f6759640e92a81fe8714ec216e75e30d5e5f8
Jahid Hasan <jahidhasan2856@gmail.com>
2022-01-29 12:59:24 +0600

    roadmap nav added

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ class MyDocument extends Document {
 
 							<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossOrigin="anonymous"></script>
 							<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossOrigin="anonymous"></script>
+							<script src="https://code.jquery.com/jquery-1.11.1.min.js" integrity="sha256-VAvG3sHdS5LqTT+5A/aeq/bZGa/Uj04xKxY8KM/w9EE=" crossOrigin="anonymous"></script>
 						</body>
 					</Html>
 					)
--- src/components/ourTeam/OurTeam.js
+++ src/components/ourTeam/OurTeam.js
@@ -13,14 +13,7 @@ const OurTeam = () => {
                 </div>
                 {/* other device  */}
                 <div className="container">
-                    <div className="
-                    text-white
-                  d-md-flex
-                  our-team-items
-                  flex-wrap
-                  justify-content-between
-                  d-none
-                ">
+                    <div className=" text-white d-md-flex our-team-items flex-wrap justify-content-between d-none">
                         <div className="our-team-item d-flex flex-column justify-content-center">
                             <div className="our-team-item-img d-flex justify-content-end flex-column">
                                 <img src="/images/team/leo-nguyen.webp" alt="" />
@@ -64,13 +57,7 @@ const OurTeam = () => {
                             <div className="position">Visual Magician</div>
                         </div>
                         <div className="our-team-item d-flex flex-column justify-content-center">
-                            <div className="
-                      our-team-item-img
-                      d-flex
-                      justify-content-end
-                      flex-column
-                      align-items-end
-                    ">
+                            <div className="  our-team-item-img d-flex justify-content-end flex-column align-items-end">
                                 <img src="/images/team/cong-an-khang.webp" alt="" />
                             </div>
                             <div className="name">CONG AN KHANG</div>
@@ -105,159 +92,23 @@ const OurTeam = () => {
                     <div className="owl-stage-outer">
                         <div className="owl-stage" style={{ transform: 'translate3d(0px, 0px, 0px)', transition: 'all 0s ease 0s' }}>
                             <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
+                                <div className="our-team-item d-flex flex-column justify-content-center  align-items-center">
+          
                                     <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/image/team/leo-nguyen.webp" alt="" />
+                                        <img src="/images/team/leo-nguyen.webp" alt="" />
                                     </div>
                                     <div className="name">Leo Nguyen</div>
                                     <div className="position">Tribal Chief</div>
                                 </div>
                             </div>
                             <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
+                                <div className="our-team-item d-flex flex-column justify-content-center  align-items-center">
+                
                                     <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/image/team/nguyen-viet-hung.webp" alt="" />
+                                        <img src="/images/team/nguyen-viet-hung.webp" alt="" />
                                     </div>
                                     <div className="name">NGUYEN VIET HUNG</div>
                                     <div className="position">Product Geek</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/nguyen-quang-thai.webp" alt="" />
-                                    </div>
-                                    <div className="name">nguyen quang thai</div>
-                                    <div className="position">Marketing Guru</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/trinh-and-duc.webp" alt="" />
-                                    </div>
-                                    <div className="name">Trinh anh duc</div>
-                                    <div className="position">The Megamind</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/hoang-duc-long.webp" alt="" />
-                                    </div>
-                                    <div className="name">Hoang duc long</div>
-                                    <div className="position">Game Master Wizard</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/duong-nhat-anh.webp" alt="" />
-                                    </div>
-                                    <div className="name">DUONG NHAT ANH</div>
-                                    <div className="position">Visual Magician</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="
-                    our-team-item-img
-                    d-flex
-                    justify-content-end
-                    flex-column
-                    align-items-end
-                  ">
-                                        <img src="/images/team/cong-an-khang.webp" alt="" />
-                                    </div>
-                                    <div className="name">CONG AN KHANG</div>
-                                    <div className="position">Design Master</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/anh-thu.webp" alt="" />
-                                    </div>
-                                    <div className="name">Anh thu</div>
-                                    <div className="position">Game Design Master</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/phan-minh-duong.webp" alt="" />
-                                    </div>
-                                    <div className="name">PHAN MINH DUONG</div>
-                                    <div className="position">Blockchain Prophet</div>
-                                </div>
-                            </div>
-                            <div className="owl-item">
-                                <div className="
-                  our-team-item
-                  d-flex
-                  flex-column
-                  justify-content-center
-                  align-items-center
-                ">
-                                    <div className="our-team-item-img d-flex justify-content-end flex-column">
-                                        <img src="/images/team/ha-trung-hieu.webp" alt="" />
-                                    </div>
-                                    <div className="name">HA TRUNG HIEU</div>
-                                    <div className="position">Blockchain Prophet</div>
                                 </div>
                             </div>
                         </div>
